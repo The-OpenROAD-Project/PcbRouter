c1685eb9ba7ccea4e300d240828e66f132eab8f7
James <k2973363@gmail.com>
2019-10-31 11:17:51 -0700

    Add Rasterizer.h again

---
+++ src/Rasterizer.h
@@ -0,0 +1,25 @@
+
+#ifndef PCBROUTER_RASTERIZER_H
+#define PCBROUTER_RASTERIZER_H
+
+#include <math.h>
+#include <stdio.h>
+#include <fstream>
+#include <iomanip>
+#include <iostream>
+#include <list>
+#include <string>
+
+using namespace std;
+
+class Rasterizer {
+   public:
+    // ctor
+    Rasterizer();
+    // dtor
+    ~Rasterizer();
+
+   private:
+};
+
+#endif
