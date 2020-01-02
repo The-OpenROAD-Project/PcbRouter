9fd13751a78ef64b9121ca182ad79fe30f1186e7
James <k2973363@gmail.com>
2020-01-03 01:17:17 +0800

    Add class header/source of GridNetclass

---
+++ src/GridNetclass.cpp
@@ -0,0 +1,106 @@
+#include "GridNetclass.h"
+
+int GridNetclass::m_obstacle_expansion = 0;
+
+void GridNetclass::setupViaIncrementalSearchGrids() {
+    this->setupIncrementalSearchGrids(this->mViaSearchingSpaceToGrids, this->mViaIncrementalSearchGrids);
+}
+void GridNetclass::setupTraceIncrementalSearchGrids() {
+    this->setupIncrementalSearchGrids(this->mTraceSearchingSpaceToGrids, this->mTraceIncrementalSearchGrids);
+}
+
+void GridNetclass::setupIncrementalSearchGrids(const std::vector<Point_2D<int>> &searchGrids, IncrementalSearchGrids &incrementalSearchGrids) {
+    // Right
+    auto searchGridsR = searchGrids;
+    // Shift the grids
+    for (auto &pt : searchGridsR) {
+        pt.m_x += 1;
+    }
+    getAddDedSearchGrids(searchGrids, searchGridsR, incrementalSearchGrids.setRightAddGrids(), incrementalSearchGrids.setRightDedGrids());
+
+    // Left
+    auto searchGridsL = searchGrids;
+    // Shift the grids
+    for (auto &pt : searchGridsL) {
+        pt.m_x -= 1;
+    }
+    getAddDedSearchGrids(searchGrids, searchGridsL, incrementalSearchGrids.setLeftAddGrids(), incrementalSearchGrids.setLeftDedGrids());
+    // // Debugging
+    // std::cout << "Left additional relative trace searching grids points: " << std::endl;
+    // for (auto &pt : addL) {
+    //     std::cout << pt << std::endl;
+    // }
+    // std::cout << "Left deduction relative trace searching grids points: " << std::endl;
+    // for (auto &pt : dedL) {
+    //     std::cout << pt << std::endl;
+    // }
+
+    // Forward
+    auto searchGridsF = searchGrids;
+    // Shift the grids
+    for (auto &pt : searchGridsF) {
+        pt.m_y += 1;
+    }
+    getAddDedSearchGrids(searchGrids, searchGridsF, incrementalSearchGrids.setForwardAddGrids(), incrementalSearchGrids.setForwardDedGrids());
+
+    // Backward
+    auto searchGridsB = searchGrids;
+    // Shift the grids
+    for (auto &pt : searchGridsB) {
+        pt.m_y -= 1;
+    }
+    getAddDedSearchGrids(searchGrids, searchGridsB, incrementalSearchGrids.setBackwardAddGrids(), incrementalSearchGrids.setBackwardDedGrids());
+
+    // LB
+    auto searchGridsLB = searchGrids;
+    // Shift the grids
+    for (auto &pt : searchGridsLB) {
+        pt.m_x -= 1;
+        pt.m_y -= 1;
+    }
+    getAddDedSearchGrids(searchGrids, searchGridsLB, incrementalSearchGrids.setLBAddGrids(), incrementalSearchGrids.setLBDedGrids());
+
+    // LF
+    auto searchGridsLF = searchGrids;
+    // Shift the grids
+    for (auto &pt : searchGridsLF) {
+        pt.m_x -= 1;
+        pt.m_y += 1;
+    }
+    getAddDedSearchGrids(searchGrids, searchGridsLF, incrementalSearchGrids.setLFAddGrids(), incrementalSearchGrids.setLFDedGrids());
+
+    // RB
+    auto searchGridsRB = searchGrids;
+    // Shift the grids
+    for (auto &pt : searchGridsRB) {
+        pt.m_x += 1;
+        pt.m_y -= 1;
+    }
+    getAddDedSearchGrids(searchGrids, searchGridsRB, incrementalSearchGrids.setRBAddGrids(), incrementalSearchGrids.setRBDedGrids());
+
+    // RF
+    auto searchGridsRF = searchGrids;
+    // Shift the grids
+    for (auto &pt : searchGridsRF) {
+        pt.m_x += 1;
+        pt.m_y += 1;
+    }
+    getAddDedSearchGrids(searchGrids, searchGridsRF, incrementalSearchGrids.setRFAddGrids(), incrementalSearchGrids.setRFDedGrids());
+}
+
+void GridNetclass::getAddDedSearchGrids(const std::vector<Point_2D<int>> &searchGrids, const std::vector<Point_2D<int>> &shiftedSearchGrids, std::vector<Point_2D<int>> &add, std::vector<Point_2D<int>> &ded) {
+    // Get add
+    for (auto &pt : shiftedSearchGrids) {
+        auto it = std::find(searchGrids.begin(), searchGrids.end(), pt);
+        if (it == searchGrids.end()) {
+            add.push_back(pt);
+        }
+    }
+    // Get ded
+    for (auto &pt : searchGrids) {
+        auto it = std::find(shiftedSearchGrids.begin(), shiftedSearchGrids.end(), pt);
+        if (it == shiftedSearchGrids.end()) {
+            ded.push_back(pt);
+        }
+    }
+}
---
+++ src/GridNetclass.h
@@ -0,0 +1,105 @@
+#ifndef PCBROUTER_GRID_NETCLASS_H
+#define PCBROUTER_GRID_NETCLASS_H
+
+#include <vector>
+#include "IncrementalSearchGrids.h"
+#include "globalParam.h"
+#include "point.h"
+
+class GridNetclass {
+   public:
+    //ctor
+    GridNetclass(const int id = -1,
+                 const int clearance = 0,
+                 const int trace_width = 0,
+                 const int via_dia = 0,
+                 const int via_drill = 0,
+                 const int uvia_dia = 0,
+                 const int uvia_drill = 0)
+        : m_id(id), m_clearance(clearance), m_trace_width(trace_width), m_via_dia(via_dia), m_via_drill(via_drill), m_uvia_dia(uvia_dia), m_uvia_drill(uvia_drill) {}
+    //dtor
+    ~GridNetclass() {}
+
+    int getId() { return m_id; }
+    int getClearance() { return m_clearance; }
+    int getDiagonalClearance() { return m_clearance_diagonal; }
+    int getTraceWidth() { return m_trace_width; }
+    int getDiagonalTraceWidth() { return m_trace_width_diagonal; }
+    int getHalfTraceWidth() { return m_half_trace_width; }
+    int getHalfDiagonalTraceWidth() { return m_half_trace_width_diagonal; }
+    int getViaDia() { return m_via_dia; }
+    int getHalfViaDia() { return m_half_via_dia; }
+    int getViaDrill() { return m_via_drill; }
+    int getMicroViaDia() { return m_uvia_dia; }
+    int getMicroViaDrill() { return m_uvia_drill; }
+    int getViaExpansion() { return m_via_expansion; }
+    static int getObstacleExpansion() { return m_obstacle_expansion; }
+    int getTraceExpansion() { return m_trace_expansion; }
+    int getDiagonalTraceExpansion() { return m_trace_expansion_diagonal; }
+    // Setup Derived
+    void setHalfTraceWidth(const int halfTraWid) { m_half_trace_width = halfTraWid; }
+    void setHalfViaDia(const int halfViaDia) { m_half_via_dia = halfViaDia; }
+    // Derived diagonal cases values
+    void setDiagonalTraceWidth(const int diagonalTraWid) { m_trace_width_diagonal = diagonalTraWid; }
+    void setHalfDiagonalTraceWidth(const int halfDiagonalTraWid) { m_half_trace_width_diagonal = halfDiagonalTraWid; }
+    void setDiagonalClearance(const int diagonalClr) { m_clearance_diagonal = diagonalClr; }
+    void setViaExpansion(const int viaExp) { m_via_expansion = viaExp; }
+    void setTraceExpansion(const int traExp) { m_trace_expansion = traExp; }
+    static void setObstacleExpansion(const int obsExp) { m_obstacle_expansion = obsExp; }
+    void setDiagonalTraceExpansion(const int traExp) { m_trace_expansion_diagonal = traExp; }
+    // Via shape
+    void addViaShapeGridPoint(const Point_2D<int> &pt) { mViaShapeToGrids.push_back(pt); }
+    void setViaShapeGrids(const std::vector<Point_2D<int>> &grids) { mViaShapeToGrids = grids; }
+    const std::vector<Point_2D<int>> &getViaShapeToGrids() const { return mViaShapeToGrids; }
+    // Trace searching space
+    void setTraceSearchingSpaceToGrids(const std::vector<Point_2D<int>> &grids) { mTraceSearchingSpaceToGrids = grids; }
+    const std::vector<Point_2D<int>> &getTraceSearchingSpaceToGrids() const { return mTraceSearchingSpaceToGrids; }
+    // Via searching space
+    void setViaSearchingSpaceToGrids(const std::vector<Point_2D<int>> &grids) { mViaSearchingSpaceToGrids = grids; }
+    const std::vector<Point_2D<int>> &getViaSearchingSpaceToGrids() const { return mViaSearchingSpaceToGrids; }
+    // Incremental searching grids
+    IncrementalSearchGrids &getTraceIncrementalSearchGrids() { return mTraceIncrementalSearchGrids; }
+    IncrementalSearchGrids &getViaIncrementalSearchGrids() { return mViaIncrementalSearchGrids; }
+    // Setup the incremental search grids
+    void setupViaIncrementalSearchGrids();
+    void setupTraceIncrementalSearchGrids();
+    void setupIncrementalSearchGrids(const std::vector<Point_2D<int>> &searchGrids, IncrementalSearchGrids &incrementalSearchGrids);
+
+   private:
+    void getAddDedSearchGrids(const std::vector<Point_2D<int>> &searchGrids, const std::vector<Point_2D<int>> &shiftedSearchGrids, std::vector<Point_2D<int>> &add, std::vector<Point_2D<int>> &ded);
+
+   private:
+    int m_id = -1;
+    int m_clearance = 0;
+    int m_trace_width = 0;
+    int m_via_dia = 0;
+    int m_via_drill = 0;
+    int m_uvia_dia = 0;
+    int m_uvia_drill = 0;
+
+    // Derived
+    int m_half_trace_width = 0;
+    int m_trace_width_diagonal = 0;
+    int m_half_trace_width_diagonal = 0;
+    int m_half_via_dia = 0;
+    int m_clearance_diagonal = 0;
+
+    int m_trace_expansion = 0;
+    int m_trace_expansion_diagonal = 0;
+    int m_via_expansion = 0;
+
+    // Refactor below static memeber....
+    static int m_obstacle_expansion;
+
+    // Via shape, relative to the via center grid
+    std::vector<Point_2D<int>> mViaShapeToGrids;
+    // Trace searching space when caluclating grid cost, relative to trace center grid
+    std::vector<Point_2D<int>> mTraceSearchingSpaceToGrids;
+    // Via searching space when caluclating grid cost, relative to via center grid
+    std::vector<Point_2D<int>> mViaSearchingSpaceToGrids;
+
+    IncrementalSearchGrids mTraceIncrementalSearchGrids;
+    IncrementalSearchGrids mViaIncrementalSearchGrids;
+};
+
+#endif
