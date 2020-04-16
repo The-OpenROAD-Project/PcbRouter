934d1c85861453563c66677fdf788be019c572ba
James <k2973363@gmail.com>
2020-04-16 13:00:25 -0700

    Update the class GridDiffPairNet

--- src/GridDiffPairNet.h
+++ src/GridDiffPairNet.h
@@ -5,13 +5,23 @@
 
 class GridDiffPairNet : public MultipinRoute {
    public:
-    GridDiffPairNet() {
-    }
-    GridDiffPairNet(const int netId, const int gridNetclassId, const size_t numGridLayer)
-        : MultipinRoute(netId, gridNetclassId, numGridLayer) {}
+    GridDiffPairNet(const int netId, const int gridNetclassId, const size_t numGridLayer,
+                    const int _gridDPNcId, MultipinRoute &net1, MultipinRoute &net2)
+        : MultipinRoute(netId, gridNetclassId, numGridLayer),
+          mGridDiffPairNetclassId(_gridDPNcId),
+          mNet1(net1),
+          mNet2(net2) {}
+
     ~GridDiffPairNet() {}
 
+    int getGridDiffPairNetclassId() const { return mGridDiffPairNetclassId; }
+    MultipinRoute &getGridNet1() const { return mNet1;}
+    MultipinRoute &getGridNet2() const { return mNet2;}
+
    private:
+    int mGridDiffPairNetclassId = -1;
+    MultipinRoute &mNet1;
+    MultipinRoute &mNet2;
 };
 
 #endif
