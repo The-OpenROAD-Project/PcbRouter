f994efa6b13645e8a85920dc559d752f44a72cb0
James <k2973363@gmail.com>
2020-04-16 21:44:25 -0700

    Update GridDiffPairNet to find median (merging) points

---
+++ src/GridDiffPairNet.cpp
@@ -0,0 +1,50 @@
+#include "GridDiffPairNet.h"
+
+void GridDiffPairNet::setupDiffPairGridPins(const int startLayerId, const int endLayerId) {
+    const std::vector<GridPin> &gridPins1 = this->mNet1.getGridPins();
+    const std::vector<GridPin> &gridPins2 = this->mNet2.getGridPins();
+
+    // if (gridPins1.size() != 2) {
+    //     std::cout << __FUNCTION__ << "(): supports only two pin nets of differential nets."
+    //               << " NetId: " << this->mNet1.getNetId() << " has net degree of " << gridPins1.size() << std::endl;
+    // }
+    // if (gridPins2.size() != 2) {
+    //     std::cout << __FUNCTION__ << "(): supports only two pin nets of differential nets."
+    //               << " NetId: " << this->mNet2.getNetId() << " has net degree of " << gridPins2.size() << std::endl;
+    // }
+
+    // For each GridPin1 find a nearest GridPin2
+    for (unsigned int i = 0; i < gridPins1.size(); ++i) {
+        const auto &gp1 = gridPins1.at(i);
+        int minHPWL = std::numeric_limits<int>::max();
+        int nearestPinId2 = -1;
+        for (unsigned int j = 0; j < gridPins2.size(); ++j) {
+            const auto &gp2 = gridPins2.at(j);
+            Location gp1center = gp1.getPinWithLayers().front();
+            Location gp2center = gp2.getPinWithLayers().front();
+
+            int HPWL = abs(gp1center.m_x - gp2center.m_x) + abs(gp1center.m_y - gp2center.m_y);
+            if (HPWL < minHPWL) {
+                minHPWL = HPWL;
+                nearestPinId2 = i;
+            }
+        }
+
+        // Record the pair
+        this->mGridPinPairsId.emplace_back(std::make_pair(i, nearestPinId2));
+        const auto &gp2 = gridPins2.at(nearestPinId2);
+
+        // Add GridPins as median (merging) points and as pseudo through-hole pin
+        GridPin &gridPin = this->getNewGridPin();
+        int medianPtX = (gp1.getPinWithLayers().front().m_x + gp2.getPinWithLayers().front().m_x) / 2;
+        int medianPtY = (gp1.getPinWithLayers().front().m_y + gp2.getPinWithLayers().front().m_y) / 2;
+
+        for (int layerId = startLayerId; layerId <= endLayerId; ++layerId) {
+            gridPin.addPinWithLayer(Location(medianPtX, medianPtY, layerId));
+        }
+
+        // Debugging
+        std::cout << "GP1: " << gp1.getPinWithLayers().front() << ", GP2: " << gp2.getPinWithLayers().front() << std::endl;
+        std::cout << "Median Point: (" << medianPtX << ", " << medianPtY << ")" << std::endl;
+    }
+}
--- src/GridDiffPairNet.h
+++ src/GridDiffPairNet.h
@@ -1,27 +1,31 @@
 #ifndef PCBROUTER_GRID_DIFF_PAIR_NET_H
 #define PCBROUTER_GRID_DIFF_PAIR_NET_H
 
+#include "GridPin.h"
 #include "MultipinRoute.h"
 
 class GridDiffPairNet : public MultipinRoute {
    public:
-    GridDiffPairNet(const int netId, const int gridNetclassId, const size_t numGridLayer,
-                    const int _gridDPNcId, MultipinRoute &net1, MultipinRoute &net2)
-        : MultipinRoute(netId, gridNetclassId, numGridLayer),
-          mGridDiffPairNetclassId(_gridDPNcId),
+    GridDiffPairNet(const int netId, const int gridDPNetclassId, const size_t numGridLayer,
+                    MultipinRoute &net1, MultipinRoute &net2)
+        : MultipinRoute(netId, gridDPNetclassId, numGridLayer),
           mNet1(net1),
           mNet2(net2) {}
 
     ~GridDiffPairNet() {}
 
-    int getGridDiffPairNetclassId() const { return mGridDiffPairNetclassId; }
-    MultipinRoute &getGridNet1() const { return mNet1;}
-    MultipinRoute &getGridNet2() const { return mNet2;}
+    int getGridDiffPairNetclassId() const { return getGridNetclassId(); }
+    MultipinRoute &getGridNet1() const { return mNet1; }
+    MultipinRoute &getGridNet2() const { return mNet2; }
+
+    void setupDiffPairGridPins(const int startLayerId, const int endLayerId);
 
    private:
-    int mGridDiffPairNetclassId = -1;
+    // int mGridDiffPairNetclassId = -1;
     MultipinRoute &mNet1;
     MultipinRoute &mNet2;
+
+    std::vector<pair<int, int>> mGridPinPairsId;  //(GridPinId1, GridPinId2)
 };
 
 #endif
