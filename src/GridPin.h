e7f68656e7c37a8092684ad596feced94529df6f
James <k2973363@gmail.com>
2020-05-08 16:01:21 -0700

    Update GridPin to support various bounding box sizes

--- src/GridPin.h
+++ src/GridPin.h
@@ -29,8 +29,12 @@ class GridPin {
 
     void setPinLL(Point_2D<int> &point) { pinLL = point; }
     void setPinUR(Point_2D<int> &point) { pinUR = point; }
+    void setExpandedPinLL(Point_2D<int> &&point) { mExpandedPinLL = point; }
+    void setExpandedPinUR(Point_2D<int> &&point) { mExpandedPinUR = point; }
+    void setContractedPinLL(Point_2D<int> &point) { mContractedPinLL = point; }
+    void setContractedPinUR(Point_2D<int> &point) { mContractedPinUR = point; }
     void setPinShape(const PinShape shape) { mPinShape = shape; }
-    void setExpandedPinPolygon(const polygon_double_t poly) { mPinPolygon = poly; }
+    void setExpandedPinPolygon(const polygon_double_t poly) { mExpandedPinPolygon = poly; }
     void setPinPolygon(const polygon_double_t poly) { mPinPolygon = poly; }
     void addPinWithLayer(const Location pt) { pinWithLayers.push_back(pt); }
     void setPinCenter(const Point_2D<int> pt) { mPinCenter = pt; }
@@ -38,9 +42,13 @@ class GridPin {
 
     const Point_2D<int> &getPinLL() const { return pinLL; }
     const Point_2D<int> &getPinUR() const { return pinUR; }
+    const Point_2D<int> &getExpandedPinLL() const { return mExpandedPinLL; }
+    const Point_2D<int> &getExpandedPinUR() const { return mExpandedPinUR; }
+    const Point_2D<int> &getContractedPinLL() const { return mContractedPinLL; }
+    const Point_2D<int> &getContractedPinUR() const { return mContractedPinUR; }
     const std::vector<Location> &getPinWithLayers() const { return pinWithLayers; }
     const std::vector<Point_2D<int>> &getPinShapeToGrids() const { return pinShapeToGrids; }
-    const polygon_double_t &getExpandedPinPolygon() const { return mPinPolygon; }
+    const polygon_double_t &getExpandedPinPolygon() const { return mExpandedPinPolygon; }
     const polygon_double_t &getPinPolygon() const { return mPinPolygon; }
     const PinShape getPinShape() const { return mPinShape; }
     const Point_2D<int> &getPinCenter() const { return mPinCenter; }
@@ -65,6 +73,13 @@ class GridPin {
     // Boost Polygon, absolute grid's coordinates
     polygon_double_t mPinPolygon;
     polygon_double_t mExpandedPinPolygon;
+
+    // align the rounded bounding box of the expanded pin polygon
+    Point_2D<int> mExpandedPinLL = Point_2D<int>(0, 0);
+    Point_2D<int> mExpandedPinUR = Point_2D<int>(0, 0);
+    // Contracted bounding box of the pin
+    Point_2D<int> mContractedPinLL = Point_2D<int>(0, 0);
+    Point_2D<int> mContractedPinUR = Point_2D<int>(0, 0);
 };
 
 #endif
